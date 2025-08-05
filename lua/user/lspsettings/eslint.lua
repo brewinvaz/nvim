7c729fd9e93fe3bd1c3019dfd1380a1d6920e872
Brewin Vaz <brewinvaz@users.noreply.github.com>
2025-08-05 13:04:04 -0400

    feat: make eslint work for monorepos

---
+++ lua/user/lspsettings/eslint.lua
@@ -0,0 +1,22 @@
+return {
+  settings = {
+    workingDirectory = { mode = "auto" },
+    format = { enable = true },
+    lint = { enable = true },
+  },
+  root_dir = function(fname)
+    -- Find the nearest eslint config file starting from the current file
+    local util = require("lspconfig").util
+    return util.root_pattern(
+      ".eslintrc.js",
+      ".eslintrc.json",
+      ".eslintrc.yaml",
+      ".eslintrc.yml",
+      ".eslintrc",
+      "eslint.config.js"
+    )(fname)
+  end,
+  -- This is crucial for monorepos
+  single_file_support = false,
+}
+
